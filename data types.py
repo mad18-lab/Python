52339a05c16909c4fbc56fb5435c6b98d0092986
Madhav Gupta <80634128+mad18-lab@users.noreply.github.com>
2023-06-10 13:19:29 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,18 @@
+x=5
+y=6.0
+z="Madhav"
+a=5+8j
+b=["This", "is", "List"]
+c={1:"This", 2:"is", 3:"dictionary"}
+d=("This", "is", "Tuple")
+e=bool(10)
+f={"This", "is", "Set"}
+print("Integer Data Type:", x)
+print("Float Data Type:", y)
+print("Complex Number Data Type:", a)
+print("String Data Type:", z)
+print("List Data Type:", b)
+print("Dictionary Data Type:", c)
+print("Tuple Data Type:", d)
+print("Set Data Type:", f)
+print("Boolean Data Type:", e)
